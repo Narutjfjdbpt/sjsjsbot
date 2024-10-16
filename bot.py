0a7c28e23d8faa5ae6107b1c13b3e583dda36da5
Narutjfjdbpt <tuffduffsuff@gmail.com>
2024-10-16 19:59:05 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
-from flask import Flask, jsonify
-import requests
-import time
 import os
+import time
+import requests
+from flask import Flask, jsonify
+from threading import Thread
 
 app = Flask(__name__)
 
@@ -14,36 +15,8 @@ def fetch_courses():
     response.raise_for_status()
     return response.json()
 
-def send_telegram_message(course):
-    image = course['image']
-    name = course['name']
-    category = course['category']
-    subcategory = course['subcategory']
-    url = course['url']
-    description = course['description']
-    
-    msg = f"<b>📚 {name}\n\n💠 Category : {category} > {subcategory}\n\n🔗 Course Link : <a href='{url}'>Click Here</a>\n\n📝 Description : <i>{description}</i></b>"
-    telegram_url = f'https://api.telegram.org/bot{bot_token}/sendPhoto'
-    
-    payload = {
-        'chat_id': chat_id,
-        'photo': image,
-        'caption': msg,
-        'parse_mode': 'HTML'
-    }
-    
-    response = requests.post(telegram_url, data=payload)
-    
-    if response.status_code != 200:
-        print(f"Failed to send message: {response.status_code} - {response.text}")
-        return False
-    
-    return True
-
 def process_courses():
     last_course = None
-
-    print("Script is running...")  # Print message when script starts
 
     while True:
         try:
@@ -58,28 +31,32 @@ def process_courses():
             }
 
             if current_course != last_course:
-                print("New course found!")
                 last_course = current_course
-                success = send_telegram_message(current_course)
-                
-                if not success:
-                    print("Error sending the message to Telegram. Retrying...")
-                    time.sleep(retry_interval)
-                    continue
 
             time.sleep(30)
 
         except Exception as e:
-            print(f"An error occurred: {e}")
             time.sleep(retry_interval)
 
 @app.route('/')
 def index():
     try:
-        print("Running the script to check for courses...")  # Output in console
-        return jsonify({"status": "Running the script to check for new courses."})  # Response to the user
+        return jsonify({"status": "Running the script to check for new courses."})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
+def run_flask():
+    app.run(host='0.0.0.0', port=8080)
+
+def run_scraper():
+    process_courses()
+
+def main():
+    flask_thread = Thread(target=run_flask)
+    flask_thread.start()
+
+    scraper_thread = Thread(target=run_scraper)
+    scraper_thread.start()
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    main()
